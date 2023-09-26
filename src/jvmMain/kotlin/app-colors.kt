2ff1dc4f14350971f68da7ea8b22329c01319c4a
wangzhen <lavalike@yeah.net>
2023-09-27 00:19:48 +0800

    增加侧边栏菜单

---
+++ src/jvmMain/kotlin/app-colors.kt
@@ -0,0 +1,9 @@
+/**
+ * app-colors
+ *
+ * @author : wangzhen
+ * @date : 2023/9/27
+ */
+const val COLOR_PANEL = 0X4DCCCCCC
+const val COLOR_PANEL_DARK = 0X66CCCCCC
+const val COLOR_TEXT = 0XFF3B424C
--- src/jvmMain/kotlin/sidebar.kt
+++ src/jvmMain/kotlin/sidebar.kt
@@ -1,8 +1,16 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,15 +29,55 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun SidebarComposable() {
-    Column(
-        modifier = Modifier.width(60.dp).fillMaxHeight().background(color = Color(0x80cccccc)).padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        modifier = Modifier.width(60.dp).fillMaxHeight().background(color = Color(COLOR_PANEL)).padding(12.dp),
     ) {
-        Image(
-            painter = painterResource("banner.jpg"),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth().aspectRatio(1f).clip(CircleShape)
-        )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box(modifier = Modifier.fillMaxWidth().aspectRatio(1f)) {
+                Image(
+                    painter = painterResource("banner.jpg"),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize().clip(CircleShape)
+                )
+                Box(
+                    modifier = Modifier.size(12.dp).clip(CircleShape)
+                        .border(border = BorderStroke(1.dp, color = Color.White), shape = CircleShape)
+                        .background(color = Color.Green).align(Alignment.BottomEnd)
+                )
+            }
+
+            Icon(
+                Icons.Default.Face,
+                contentDescription = null,
+                tint = Color(COLOR_TEXT),
+                modifier = Modifier.fillMaxWidth().padding(top = 20.dp).clip(RoundedCornerShape(10.dp))
+                    .background(color = Color(COLOR_PANEL_DARK)).aspectRatio(1f).padding(8.dp)
+            )
+
+            Icon(
+                Icons.Default.Person,
+                contentDescription = null,
+                tint = Color(COLOR_TEXT),
+                modifier = Modifier.fillMaxWidth().padding(top = 10.dp).clip(RoundedCornerShape(10.dp))
+                    .background(color = Color(COLOR_PANEL_DARK)).aspectRatio(1f).padding(8.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                Icons.Default.Settings,
+                contentDescription = null,
+                tint = Color(COLOR_TEXT),
+                modifier = Modifier.fillMaxWidth().aspectRatio(1f).clip(RoundedCornerShape(10.dp))
+                    .background(color = Color(COLOR_PANEL_DARK)).padding(8.dp)
+            )
+        }
     }
 }
